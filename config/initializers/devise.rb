a5e59333a1bb829db71e1a82691519333242895b
Pedro <nebewu@gmail.com>
2022-06-05 16:13:22 -0300

    Implement part2 code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,8 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # Solution to undefined method `user_url' for #<RegistrationsController:0x0000000000d4a8>
+  #            recipient.public_send(method, *args)
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 end
